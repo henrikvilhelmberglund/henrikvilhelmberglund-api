feb63d186caa0a4c630c361e40138850ff1bd367
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-10 16:47:37 +0200

    feat: :sparkles: add Nav to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script>
 	import "@unocss/reset/antfu.css";
+	import Nav from "$lib/Nav.svelte";
+	const routes = [{ link: "api-documentation", display: "API Documentation" }];
 </script>
+
+<Nav {routes} level={1} />
 
 <slot />
 
