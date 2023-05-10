e3b2f858c72e18e9bada18c076bde664318f64b0
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-10 16:47:13 +0200

    feat: :sparkles: add tag getting function to API

--- src/routes/api/posts/+server.js
+++ src/routes/api/posts/+server.js
@@ -8,11 +8,19 @@ export async function GET({ url, request }) {
 	let result;
 	const limit = parseInt(url.searchParams.get("limit", 10));
 	const latest = url.searchParams.get("latest");
+	const tag = url.searchParams.get("tag");
 
 	if (latest) {
-		result = await postsCollection.find({}).sort({ _id: -1 }).limit(parseInt(limit, 10)).toArray();
+		result = await postsCollection
+			.find(tag ? { tags: tag } : {})
+			.sort({ _id: -1 })
+			.limit(parseInt(limit, 10))
+			.toArray();
 	} else {
-		result = await postsCollection.find({}).limit(limit).toArray();
+		result = await postsCollection
+			.find(tag ? { tags: tag } : {})
+			.limit(limit)
+			.toArray();
 	}
 
 	return json({
