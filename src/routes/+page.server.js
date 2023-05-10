899f7ba29eeb9e63d376e7790df3d81c6c09ccf0
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-10 16:47:51 +0200

    feat: :sparkles: add index route and dynamic tag route

---
+++ src/routes/+page.server.js
@@ -0,0 +1,20 @@
+import { getPostsCollection } from "../hooks.server";
+/** @type {import('./$types').PageServerLoad} */
+export async function load() {
+	const postsCollection = await getPostsCollection();
+	let result;
+	const latest = true;
+	const limit = 0;
+
+	if (latest) {
+		result = await postsCollection.find({}).sort({ _id: -1 }).limit(parseInt(limit, 10)).toArray();
+	} else {
+		result = await postsCollection.find({}).limit(limit).toArray();
+	}
+
+	result.forEach((item) => {
+		item._id = item._id.toString();
+	});
+
+	return { posts: result };
+}
